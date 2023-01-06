65789d889ea92f9192436f120d49a311a800baaf
dtmiranda <hortadito2@gmail.com>
2023-01-06 20:24:02 -0100

    refactor: add mongodb connection and express json midware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express' 
+import { ConnectDb } from './database/connect';
 import { routes } from "./routes/routes"
+
+
 
 const PORT = process.env.PORT || 3333; 
 
 
 const server = express()
+
+server.use(express.json())
 
 server.use(routes)
 
@@ -12,6 +17,9 @@ const startServer = () => {
   try {
     server.listen(PORT)
     console.log("Server is running on PORT: ",  PORT)
+
+    ConnectDb("mongodb+srv://sam:1234@cluster0.m0zii1h.mongodb.net/article_db")
+
 
   } catch (error) {
     console.log(error)
